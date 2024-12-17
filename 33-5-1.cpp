ca85a7f02fbd6d80577cf5969d2c46615816fa61
ZSergeyN <zsergeyn@outlook.com>
2024-12-17 21:46:31 +0600

    finish

--- 33-5-1.cpp
+++ 33-5-1.cpp
@@ -4,41 +4,64 @@
 
 void initBase(std::map<std::string, int>& product) {
     bool over_init = false;
-    while (!over_init) {
+    while(!over_init) {
         std::string art;
         int count;
         std::cout << "Enter the product (x - end)" << std::endl << " article: ";
         std::cin >> art;
-        if (art == "x") {
+        if(art == "x") {
             over_init = true;
-        }
-        else {
+        } else {
             std::cout << "quantity: ";
             std::cin >> count;
-            product.insert(std::pair<std::string, int>(art, count));
+            product.insert(std::pair<std::string, int> (art, count));
         }
     }
 };
 
+void checkPoduct (std::map<std::string, int>& base, std::string _art, int _count) {
+    auto search = base.find(_art);
+    if(search == base.end()) {
+        throw std::invalid_argument("The article is missing");
+    }
+    if(base.at(_art) < _count) {
+        throw std::invalid_argument("The quantity is missing");
+    }
+}
+
 void printList(std::map<std::string, int>& _list) {
     std::map<std::string, int>::iterator it;
-    for (it = _list.begin(); it != _list.end(); ++it) {
+    for(it = _list.begin(); it != _list.end(); ++it) {
         std::cout << it->first << " : " << it->second << std::endl;
     }
 }
 
 void addProduct(std::map<std::string, int>& _product, std::map<std::string, int>& _basket, std::string _art, int _count) {
-    _basket.insert(std::pair<std::string, int>(_art, _count));
+    try{
+        checkPoduct(_product, _art, _count);
+        _basket.insert(std::pair<std::string, int> (_art, _count));
+    }
+    catch (const std::invalid_argument &er) {
+        std::cerr << "Cought exeption: " << er.what() << std::endl;
+    }
+    catch (const std::runtime_error& er) {
+        std::cerr << "Cought exeption: " << er.what() << std::endl;
+    }
 }
 
 void removeProduct(std::map<std::string, int>& _basket, std::string _art, int _count) {
-    auto search = _basket.find(_art);
-    if (search != _basket.end()) {
+    try {
+        checkPoduct(_basket, _art, _count);
         _basket.at(_art) -= _count;
+        if (_basket.at(_art) == 0) { // Delete element if value = 0
+            _basket.erase(_art);
+        }
     }
-    else {
-        std::cout << "Nothing" << std::endl;
-        //_product.insert(std::pair<std::string, int> (_art, _count));
+    catch (const std::invalid_argument& er) {
+        std::cerr << "Cought exeption: " << er.what() << std::endl;
+    }
+    catch (const std::runtime_error& er) {
+        std::cerr << "Cought exeption: " << er.what() << std::endl;
     }
 };
 
@@ -46,27 +69,25 @@ int main() {
     std::map<std::string, int> product, basket;
     initBase(product);
     //printList(product);
-    while (true) {
+    bool action_in_basket = true;
+    while(action_in_basket) {
         std::string action;
-        std::cout << "Enter the action (add, remove, print, end): ";
+        std::cout <<"Enter the action (add, remove, print, end): ";
         std::cin >> action;
-        if (action == "end") {
-            break;
-        }
-        else if (action == "print") {
+        if(action == "end") {
+            action_in_basket = false;
+        } else if(action == "print") {
             printList(basket);
-        }
-        else {
+        } else if(action == "add" || action == "remove") {
             std::string art;
             int count;
             std::cout << "\tarticle: ";
             std::cin >> art;
             std::cout << "\tquantity: ";
             std::cin >> count;
-            if (action == "add") {
+            if(action == "add") {
                 addProduct(product, basket, art, count);
-            }
-            else if (action == "remove") {
+            } else if(action == "remove") {
                 removeProduct(basket, art, count);
             }
         }
